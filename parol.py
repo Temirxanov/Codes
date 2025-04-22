b68c08d78ff1d4e65ce19ee2c0fabdcf7cb1a1e8
PYTHON HTML CSS <temirxanovarziwbek2009@gmail.com>
2025-04-22 16:22:10 +0500

    Update parol.py
    Login qosildi

--- parol.py
+++ parol.py
@@ -2,16 +2,24 @@ from logging.handlers import TimedRotatingFileHandler
 import logging
 logger = logging.getLogger("logger_fayl")
 logger.setLevel(logging.INFO)
+loginn = 'temirxanov'
+login = input("Login:")
 parol = ' 12345678'
 soraw = input("Parol jazin : ")
 sanaw = 0
-if parol != parol:
-    ozgeriwshi = TimedRotatingFileHandler('logs',when="midnight",interval=1,backupCount=7)
+if parol != parol or login != loginn:
+    ozgeriwshi = TimedRotatingFileHandler('logss',when="midnight",interval=1,backupCount=7)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     ozgeriwshi.setFormatter(formatter)
 
     logger.addHandler(ozgeriwshi)
-    logger.info(f"{sanaw} ret qate kiritdin Qate parol\n{sanaw} ret urindin")
+    if parol!=soraw and login == loginn:
+        logger.info(f"Parol qate")
+    elif parol == soraw and login != loginn:
+        parol!=soraw and login == loginn
+    else:
+        logger.info("Login ham parolde qate")
 else:
     print("parol duris")
+    
     
